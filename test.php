0f88436612f121afceaeebcb2a0bb14b067116e7
Marc Coll <marc@marc-coll.com>
2016-05-18 16:08:47 +0200

    adding klarna connection test

--- test.php
+++ test.php
@@ -1,15 +1,32 @@
 <?php
 
 require_once 'config.php';
-require_once 'LogClass.php';
+require_once 'Klarna/Checkout.php';
 
-require_once $magePath;
-umask(0);
+$klarna_order = $_GET['klarna_order'];
 
+// set env credentials
+if($klara_env == 'test'){
+  $klarna_url = Klarna_Checkout_Connector::BASE_TEST_URL;
+  $klarna_secret = $test_klarna_secret;
+}else{
+  $klarna_url = Klarna_Checkout_Connector::BASE_URL;
+  $klarna_secret = $prod_klarna_secret;
+}
 
-$storeID = $_GET['storeID'];
-if($storeID){
-  echo $storeID;
-}else {
-  echo 'no id';
+// get klarna order
+$connector = Klarna_Checkout_Connector::create(
+    base64_encode(hash('sha256', $klarna_secret, true)),
+    $klarna_url
+);
+
+$order = new Klarna_Checkout_Order($connector, $klarna_order);
+try {
+  $order->fetch();
+  echo 'connection ok <br />';
+  echo $order['status'];
+} catch (Klarna_Checkout_ApiErrorException $e) {
+  var_dump($e->getMessage());
+  var_dump($e->getPayload());
+  die;
 }
